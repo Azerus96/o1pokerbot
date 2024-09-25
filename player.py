3c2c79219e64939ef926143b8b4fc99532462a1a
Azerus96 <gencinski1996@gmail.com>
2024-09-25 09:35:22 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,6 @@
 from mccfr import MCCFR
+import pickle
+import random
 
 class PokerPlayer:
     def __init__(self, name, stack, use_mccfr=True, iterations=1000):
@@ -11,45 +13,43 @@ class PokerPlayer:
 
         self.strategy_system = MCCFR(iterations) if use_mccfr else BasicPokerStrategy()
 
-    # Добавьте остальные методы, необходимые для вашего игрока
-self.strategy_system = MCCFR(iterations) if use_mccfr else BasicPokerStrategy()
+    def make_decision(self, game_state):
+        decision = self.strategy_system.decide(game_state)
+        self.store_decision(game_state, decision)
+        return decision
 
-def make_decision(self, game_state):
-    decision = self.strategy_system.decide(game_state)
-    self.store_decision(game_state, decision)
-    return decision
+    def store_decision(self, game_state, decision):
+        self.history.append({
+            "game_state": game_state,
+            "decision": decision
+        })
 
-def store_decision(self, game_state, decision):
-    self.history.append({
-        "game_state": game_state,
-        "decision": decision
-    })
+    def record_opponent_action(self, opponent_name, action):
+        if opponent_name not in self.dossier:
+            self.dossier[opponent_name] = {"aggro": 0, "fold": 0, "call": 0, "bluff": 0}
+        self.dossier[opponent_name][action] += 1
 
-def record_opponent_action(self, opponent_name, action):
-    if opponent_name not in self.dossier:
-        self.dossier[opponent_name] = {"aggro": 0, "fold": 0, "call": 0, "bluff": 0}
-    self.dossier[opponent_name][action] += 1
+    def adjust_strategy(self):
+        if self.use_mccfr:
+            self.strategy_system.run_iterations(self.history)
 
-def adjust_strategy(self):
-    if self.use_mccfr:
-        self.strategy_system.run_iterations(self.history)
+    def save_state(self, file_name=None):
+        if file_name is None:
+            file_name = f'{self.name}_state.pkl'
+        with open(file_name, 'wb') as file:
+            pickle.dump({
+                "history": self.history,
+                "dossier": self.dossier,
+                "strategy": self.strategy_system.strategy
+            }, file)
 
-def save_state(self, file_name=None):
-    if file_name is None:
-        file_name = f'{self.name}_state.pkl'
-    with open(file_name, 'wb') as file:
-        pickle.dump({
-            "history": self.history,
-            "dossier": self.dossier,
-            "strategy": self.strategy_system.strategy
-        }, file)
+    def load_state(self, file_name):
+        with open(file_name, 'rb') as file:
+            state = pickle.load(file)
+            self.history = state["history"]
+            self.dossier = state["dossier"]
+            self.strategy_system.strategy = state["strategy"]
 
-def load_state(self, file_name):
-    with open(file_name, 'rb') as file:
-        state = pickle.load(file)
-        self.history = state["history"]
-        self.dossier = state["dossier"]
-        self.strategy_system.strategy = state["strategy"]
-        class BasicPokerStrategy:
-def decide(self, game_state):
-return random.choice(['fold', 'call', 'raise'])
+class BasicPokerStrategy:
+    def decide(self, game_state):
+        return random.choice(['fold', 'call', 'raise'])
