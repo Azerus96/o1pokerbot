760d3e594c014575dfeeac10efb9f06286c5bf11
Azerus96 <gencinski1996@gmail.com>
2024-09-25 09:54:42 +0300

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO
 from player import PokerPlayer
-from poker_game import PokerGame, setup_tournament
+from poker_game import setup_tournament
 from mccfr import MCCFR
 from logging_system import Logger
 import os
@@ -9,7 +9,6 @@ import os
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Создаём объект турнира (глобальная переменная)
 tournament = None
 logger = Logger()
 
@@ -31,22 +30,29 @@ def register():
         tournament.players.append(new_player)
         logger.log_event(f"Player {player_name} зарегистрирован.")
         return redirect('/tournament')
-
     return render_template('player.html')
 
 @app.route('/start_tournament', methods=['POST'])
 def start_tournament():
-    global tournament  # Важно указать, что мы используем глобальную переменную
+    global tournament
     max_players = int(request.form['max_players'])
     bot_players = int(request.form['bot_players'])
     initial_stack = int(request.form['initial_stack'])
     round_duration = int(request.form['round_duration'])
-    payout_structure = int(request.form['payouts'])
+    payout_structure = request.form['payouts']
     allow_late_registration = 'allow_late_registration' in request.form
     late_registration_rounds = int(request.form['late_registration_rounds'])
 
-    # Настройка и запуск турнира с полученными параметрами
-    tournament = setup_tournament(max_players, bot_players, initial_stack, round_duration, payout_structure, allow_late_registration, late_registration_rounds)
+    tournament = setup_tournament(
+        max_players=max_players,
+        bot_players=bot_players,
+        initial_stack=initial_stack,
+        round_duration=round_duration,
+        payout_structure=payout_structure,
+        allow_late_registration=allow_late_registration,
+        late_registration_rounds=late_registration_rounds,
+        mccfr_strategy=mccfr_strategy
+    )
     logger.log_event("Турнир начат с заданными параметрами.")
     return redirect('/tournament')
 
@@ -80,7 +86,15 @@ if __name__ == "__main__":
     if os.path.exists('mccfr_strategy.pkl'):
         mccfr_strategy.load_strategy('mccfr_strategy.pkl')
 
-    tournament = setup_tournament(num_players=160, mccfr_strategy=mccfr_strategy)
+    tournament = setup_tournament(
+        max_players=160,
+        bot_players=10,
+        initial_stack=10000,
+        round_duration=6,
+        payout_structure='standard',
+        allow_late_registration=True,
+        late_registration_rounds=2,
+        mccfr_strategy=mccfr_strategy
+    )
 
-    # Запуск веб-сервера для взаимодействия через браузер
     start_flask()
